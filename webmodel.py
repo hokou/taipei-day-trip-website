bc32aaec47c2d6818b53ebdb6a85f35bf44d35c9
hokou.shiau <hokou.shiau@gmail.com>
2021-04-25 14:44:02 +0800

    add webmodel

---
+++ webmodel.py
@@ -0,0 +1,32 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+# print(dir(db))
+
+class Attraction(db.Model):
+    __tablename__ = 'attraction_data'
+    id = db.Column(db.BigInteger, autoincrement=True, primary_key=True)
+    rowid = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.VARCHAR(255), nullable=False)
+    category = db.Column(db.VARCHAR(255), nullable=True)
+    description = db.Column(db.VARCHAR(2000), nullable=True)
+    address = db.Column(db.VARCHAR(255), nullable=True)
+    transport = db.Column(db.VARCHAR(2000), nullable=True)
+    mrt = db.Column(db.VARCHAR(2000), nullable=True)
+    latitude = db.Column(db.Float, nullable=True)
+    longitude = db.Column(db.Float, nullable=True)
+    images = db.Column(db.VARCHAR(4000), nullable=True)
+    time = db.Column(db.DateTime, default=datetime.now)
+
+    def __init__(self, rowid, name, category, description, address, transport, mrt, latitude, longitude, images):
+        self.rowid = rowid
+        self.name = name
+        self.category = category
+        self.description = description
+        self.address = address
+        self.transport = transport
+        self.mrt = mrt
+        self.latitude = latitude
+        self.longitude = longitude
+        self.images = images
