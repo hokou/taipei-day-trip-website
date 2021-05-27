798766a133c41577c8cdad7692e8722d5c8163cb
hokou.shiau <hokou.shiau@gmail.com>
2021-05-28 00:10:15 +0800

    增加 login mark

---
+++ static/js/base.js
@@ -0,0 +1,9 @@
+
+let login_signup = document.getElementById("login_signup");
+let login_mark = document.querySelector(".login_mark");
+
+login_signup.addEventListener("click", show)
+
+function show() {
+    login_mark.style.display = "block";
+}
