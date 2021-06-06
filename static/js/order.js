737dba4bc031e98a1f7d4a9fb9b09b143141ff80
hokou.shiau <hokou.shiau@gmail.com>
2021-06-06 16:27:45 +0800

    增加 order API

---
+++ static/js/order.js
@@ -0,0 +1,67 @@
+// order
+let appID = 20483;
+let appKey = 'app_QLFiqzrrn2UCRz37LDZ3vBwNW7A5RdXoLifptkVFd8j9HPialfsBOD5kkwwz';
+let serverType = "sandbox";
+
+TPDirect.setupSDK(appID, appKey, serverType);
+
+let fields = {
+    number: {
+        // css selector
+        element: '#card-number',
+        placeholder: '**** **** **** ****'
+    },
+    expirationDate: {
+        // DOM object
+        element: document.getElementById('card-expiration-date'),
+        placeholder: 'MM / YY'
+    },
+    ccv: {
+        element: '#card-ccv',
+        placeholder: 'ccv'
+    }
+}
+
+TPDirect.card.setup({
+    // Display ccv field
+    fields: fields,
+    styles: {
+        // Style all elements
+        'input': {
+            'color': 'gray'
+        },
+        // Styling ccv field
+        'input.ccv': {
+            // 'font-size': '16px'
+        },
+        // Styling expiration-date field
+        'input.expiration-date': {
+            // 'font-size': '16px'
+        },
+        // Styling card-number field
+        'input.card-number': {
+            // 'font-size': '16px'
+        },
+        // style focus state
+        ':focus': {
+            // 'color': 'black'
+        },
+        // style valid state
+        '.valid': {
+            'color': 'green'
+        },
+        // style invalid state
+        '.invalid': {
+            'color': 'red'
+        },
+        // Media queries
+        // Note that these apply to the iframe, not the root window.
+        '@media screen and (max-width: 400px)': {
+            'input': {
+                'color': 'orange'
+            }
+        }
+    }
+})
+
+
